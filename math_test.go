a131087e01c60c3939cce3f4aff423faac6b2bbe
Lucas <lucas.2097@gmail.com>
2022-09-08 01:52:41 -0300

    test: add tests for basic math operations

--- math_test.go
+++ math_test.go
@@ -9,3 +9,30 @@ func TestSoma(t *testing.T) {
 		t.Errorf("Resultado da soma é inválido: Resultado %d. Esperado: %d", total, 30)
 	}
 }
+
+
+func TestSubtracao(t *testing.T) {
+	total := Subtrair(15, 15)
+
+	if total != 0 {
+		t.Errorf("Resultado da soma é inválido: Resultado %d. Esperado: %d", total, 0)
+	}
+}
+
+
+func TestMultiplicacao(t *testing.T) {
+	total := Multiplicar(15, 2)
+
+	if total != 30 {
+		t.Errorf("Resultado da soma é inválido: Resultado %d. Esperado: %d", total, 30)
+	}
+}
+
+
+func TestDivisao(t *testing.T) {
+	total := Dividir(10, 2)
+
+	if total != 5 {
+		t.Errorf("Resultado da soma é inválido: Resultado %d. Esperado: %d", total, 5)
+	}
+}
